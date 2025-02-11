5fed4c4a71cee594dfd3d900d83a51e97e62ddf3
SahasraLagudu <lagudusahasra@gmail.com>
2025-02-11 14:04:47 -0600

    Linked forms

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,48 +31,102 @@
             this.upload = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // upload
             // 
-            this.upload.Location = new System.Drawing.Point(75, 53);
+            this.upload.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.upload.Location = new System.Drawing.Point(56, 418);
+            this.upload.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.upload.Name = "upload";
-            this.upload.Size = new System.Drawing.Size(194, 80);
+            this.upload.Size = new System.Drawing.Size(146, 65);
             this.upload.TabIndex = 0;
-            this.upload.Text = "New Button";
+            this.upload.Text = "Graphs or Calculate Risk Score";
             this.upload.UseVisualStyleBackColor = true;
             this.upload.Click += new System.EventHandler(this.button1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(75, 356);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(56, 265);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 80);
+            this.button1.Size = new System.Drawing.Size(146, 65);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Calculate Risk Score";
+            this.button1.Text = "Modify Patient Details";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(75, 200);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(56, 127);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(194, 80);
+            this.button2.Size = new System.Drawing.Size(146, 65);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Modify Patient Details";
+            this.button2.Text = "Import Preliminary Files";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 371);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(340, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Click below if you\'d like to make a new graph or calculate a risk score: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(298, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Click below if you have any updates to make to Patient Data: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(365, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Have new symptom or disease updates to make? Click on the button below:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(155, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(165, 31);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Home Page";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(828, 508);
+            this.ClientSize = new System.Drawing.Size(489, 523);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.upload);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,6 +135,10 @@
         private System.Windows.Forms.Button upload;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
---
+++ Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VirusTrackerCode
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1(); //How do you close this one and go back to your existing form?
+            f1.Show(); 
+        }
+    }
+}
